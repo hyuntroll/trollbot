4e69af968656a77237b3f30bbd6bdd44859a0b50
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2021-04-24 00:10:05 +0900

    Update ㅋㅋㄹㅃㅃ.py

--- ㅋㅋㄹㅃㅃ.py
+++ ㅋㅋㄹㅃㅃ.py
@@ -1,2 +1,19 @@
-sbas
-!
+import discord
+
+client = discord.Cient()
+
+
+
+@Client.event
+async def on redy():
+  prinnt(client.user.id)
+  print(client.user.id)
+  print("ready")
+ 
+@client.event
+async def on_message(message):
+  if message.content.startsyith('안녕친구"):
+      awit message.channel.send("gktpdy"):
+
+                                
+client.run("ODEyOTIyNTIzOTI2NjU5MDgy.YDHzGQ.dHfdl3RMRFX3PYVHGJRBAyNRS-8")
