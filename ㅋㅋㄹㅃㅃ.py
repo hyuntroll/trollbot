905100aec250ec1319e08238d1ccc366cfe2f976
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2021-04-24 00:12:40 +0900

    Update ㅋㅋㄹㅃㅃ.py

--- ㅋㅋㄹㅃㅃ.py
+++ ㅋㅋㄹㅃㅃ.py
@@ -1,4 +1,5 @@
 import discord
+import os
 
 client = discord.Cient()
 
@@ -14,6 +15,7 @@ async def on redy():
 async def on_message(message):
   if message.content.startsyith('안녕친구"):
       awit message.channel.send("gktpdy"):
-
                                 
-client.run("ODEyOTIyNTIzOTI2NjU5MDgy.YDHzGQ.dHfdl3RMRFX3PYVHGJRBAyNRS-8")
+
+access_token = os.environ["BOT_TOKEN"]                         
+client.run("access_token")
